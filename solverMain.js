7512278b86aa1bbe7b77478305aeb72235602a2b
Dmitry L <58388093+dphdmn@users.noreply.github.com>
2023-10-27 05:45:53 +0800

    validation

--- solverMain.js
+++ solverMain.js
@@ -41,6 +41,30 @@ class Solver{
       return this.promise;
   }
 }
+
+function validateScramble(input) {
+    // Check if the input contains only numbers, spaces, or '/'
+    if (!/^[0-9\s/]*$/.test(input)) {
+      return false;
+    }
+  
+    // Split the input string by '/' to get parts
+    const parts = input.split('/');
+  
+    // Check if each part has the same amount of numbers
+    const numCounts = parts.map(part => part.split(' ').length);
+    const allEqual = numCounts.every(count => count === numCounts[0]);
+  
+    // Check if the sorted array of all numbers is sequential
+    const allNumbers = input.split(/\s|\/| /).map(Number);
+    const sortedNumbers = [...allNumbers].sort((a, b) => a - b);
+    const isSequential = sortedNumbers.every((num, index) => num === index);
+  
+    // Return true if both conditions are met, otherwise return false
+    return allEqual && isSequential;
+  }
+  
+
 var solver = new Solver();
 async function solveScramble(scramble){
   if(!solver.ready) return [];
@@ -51,14 +75,19 @@ scrambleInput.addEventListener('input', () => {
     resultDisplay.innerHTML = '';
 });
 scrambleInput.addEventListener('change', () => {
-  const scramble = scrambleInput.value;
+  let scramble = scrambleInput.value;
   resultDisplay.innerHTML = 'Solving...';
   if (scramble) {
-    solveScramble(scramble).then((result) => {
-        const resultsList = result.split('\n');
+    scramble = scramble.replace(/\s+/g, ' ').trim();
+    if (validateScramble(scramble) && scramble.length === 37){
+        solveScramble(scramble).then((result) => {
+            const resultsList = result.split('\n');
 
-        resultDisplay.innerHTML = `${resultsList.length} solutions found<br>${resultsList[0].length} moves optimal length<br>${resultsList.join('<br>')}`;  
-    });
+            resultDisplay.innerHTML = `${resultsList.length} solutions found<br>${resultsList[0].length} moves optimal length<br>${resultsList.join('<br>')}`;  
+        });
+    } else{
+        resultDisplay.innerHTML = 'Bad scramble format.';
+    }
   } else {
     resultDisplay.innerHTML = '';
   }
